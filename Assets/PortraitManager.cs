49aa117cf3d3eead5320780000ee7c4c193ef698
Javier <35javier35@gmail.com>
2018-09-02 14:03:58 -0600

    Added Character Select Screen

---
+++ Assets/PortraitManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortraitManager : MonoBehaviour {
+
+	public int defaultPortrait = 0;
+
+	SpriteRenderer portraitRenderer;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		portraitRenderer = GetComponentInChildren<SpriteRenderer>();
+		anim = GetComponent<Animator>();
+	}
+
+	public void join(Sprite portrait){
+		portraitRenderer.sprite =portrait;
+		anim.SetTrigger("Join");
+	}
+
+	public void leave(){
+		anim.SetTrigger("Leave");
+		portraitRenderer.sprite = null;
+	}
+
+	public void switchPortrait(Sprite portrait){
+		portraitRenderer.sprite =portrait;
+	}
+}
