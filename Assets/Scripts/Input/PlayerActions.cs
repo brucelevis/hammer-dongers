85b400940fe62568952b128762e291af9b100e36
bnapky <napky33@gmail.com>
2018-09-02 02:13:29 -0600

    Player Inctrontol Actions

---
+++ Assets/Scripts/Input/PlayerActions.cs
@@ -0,0 +1,76 @@
+﻿using InControl;
+
+public class PlayerActions : PlayerActionSet
+{
+	public PlayerAction Left;
+	public PlayerAction Right;
+	public PlayerAction Up;
+	public PlayerAction Down;
+	public PlayerAction Smash;
+	public PlayerAction Dash;
+	public PlayerTwoAxisAction Move;
+
+	public PlayerActions()
+	{
+		Dash = CreatePlayerAction( "Dash" );
+		Smash = CreatePlayerAction( "Smash" );
+		Up = CreatePlayerAction( "Up" );
+		Down = CreatePlayerAction( "Down" );
+		Left = CreatePlayerAction( "Left" );
+		Right = CreatePlayerAction( "Right" );
+		Move = CreateTwoAxisPlayerAction( Left, Right, Down, Up );
+	}
+
+
+	public static PlayerActions CreateWithKeyboardBindingsForPlayerOne()
+	{
+		var actions = new PlayerActions();
+
+		actions.Up.AddDefaultBinding( Key.W );
+		actions.Down.AddDefaultBinding( Key.S );
+		actions.Right.AddDefaultBinding( Key.D );
+		actions.Left.AddDefaultBinding( Key.A );
+
+		actions.Smash.AddDefaultBinding( Key.LeftShift);
+		actions.Dash.AddDefaultBinding( Key.Shift );
+
+		return actions;
+	}
+
+	public static PlayerActions CreateWithKeyboardBindingsForPlayerTwo()
+	{
+		var actions = new PlayerActions();
+
+		actions.Up.AddDefaultBinding( Key.UpArrow );
+		actions.Down.AddDefaultBinding( Key.DownArrow );
+		actions.Right.AddDefaultBinding( Key.RightArrow );
+		actions.Left.AddDefaultBinding( Key.LeftArrow );
+
+		actions.Smash.AddDefaultBinding( Key.Return );
+		actions.Dash.AddDefaultBinding( Key.RightShift );
+
+		return actions;
+	}
+
+	public static PlayerActions CreateWithJoystickBindings()
+	{
+		var actions = new PlayerActions();
+
+		actions.Smash.AddDefaultBinding( InputControlType.Action1 );
+		actions.Dash.AddDefaultBinding( InputControlType.Action2 );
+
+		actions.Up.AddDefaultBinding( InputControlType.LeftStickUp );
+		actions.Down.AddDefaultBinding( InputControlType.LeftStickDown );
+		actions.Left.AddDefaultBinding( InputControlType.LeftStickLeft );
+		actions.Right.AddDefaultBinding( InputControlType.LeftStickRight );
+
+		actions.Up.AddDefaultBinding( InputControlType.DPadUp );
+		actions.Down.AddDefaultBinding( InputControlType.DPadDown );
+		actions.Left.AddDefaultBinding( InputControlType.DPadLeft );
+		actions.Right.AddDefaultBinding( InputControlType.DPadRight );
+
+		return actions;
+	}
+}
+
+
---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerInput : MonoBehaviour {
+	float magnitudeX = 0;
+	float magnitudeY = 0;
+
+	float lastDirectionX = 0;
+	float lastDirectionY = -1;
+
+	public int PlayerPrefix   = 1;
+	PlayerBehaviour player;
+	public PlayerActions Actions { get; set; }
+
+	void Start () {
+		player = GetComponent<PlayerBehaviour> ();
+	}
+	
+	void Update () {
+		if (Actions == null)
+			return;
+		
+		if(Time.timeSinceLevelLoad > 1)
+			InterpretInput();
+	}
+		
+	void FixedUpdate () {
+		if (magnitudeX != 0 || magnitudeY != 0) {
+			lastDirectionX = magnitudeX;
+			lastDirectionY = magnitudeY;
+		}
+		player.Move (new Vector2(magnitudeX, magnitudeY));
+	}
+
+	private void InterpretInput () {
+		if (Actions.Smash.WasPressed) {
+			player.Hit ();
+			magnitudeY = 0;
+			magnitudeX = 0;
+			return;
+		}
+
+		magnitudeY = Actions.Move.Y;
+		magnitudeX = Actions.Move.X;
+
+		if (Actions.Dash.WasPressed) 
+			player.StartDash (lastDirectionX, lastDirectionY);
+	}
+}
