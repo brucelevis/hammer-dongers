19cc488b985467e19592c8a47d54f6e294b0590f
bnapky <napky33@gmail.com>
2018-09-02 22:14:40 -0600

    update

--- Assets/Scripts/MatchManager.cs
+++ Assets/Scripts/MatchManager.cs
@@ -75,7 +75,7 @@ public class MatchManager : MonoBehaviour {
 			scores [playerPrefix]++;
 			UpdateScoreInUI(playerPrefix, ""+scores [playerPrefix]);
 
-			if (scores [playerPrefix] == 1) {
+			if (scores [playerPrefix] == 3) {
 				SetVictoryMessage (playerPrefix);
 				return;
 			}
