4e7dd7fead5e00082f3ab7806f79798ba63b5cfb
Javier <35javier35@gmail.com>
2018-09-04 00:25:25 -0600

    Ready Message

---
+++ Assets/CharacterSelectTitleBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectTitleBehaviour : MonoBehaviour {
+
+	GameObject banners;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		banners = GameObject.Find("CharacterSlots");
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		var portraits = banners.GetComponentsInChildren(typeof(PortraitManager), true);
+		
+		int takenSpots = 0;
+
+		foreach(PortraitManager portrait in portraits){
+			if(!portrait.available)
+				takenSpots++;
+		}
+
+		Debug.Log(takenSpots);
+
+		if(takenSpots >= 2)
+			anim.SetBool("Ready", true);
+		else
+			anim.SetBool("Ready", false);
+	}
+}
